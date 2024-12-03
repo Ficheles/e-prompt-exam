f58c36b30a9a39f0fb41ad40a5526a351ae27515
rafaelcalassara <rafaelcalassara@hotmail.com>
2024-12-02 22:32:31 -0300

    initial

---
+++ prompt.py
@@ -0,0 +1,51 @@
+PERSONA = """Você é um especialista em análise de consumo de cartões de crédito com vasta experiência em classificação e categorização de despesas financeiras.
+Sua tarefa é analisar os dados em formato CSV que contem os detalhes das transações da fatura em <fatura_csv>,
+identificar e categorizar cada item de acordo com a lista de categorias fornecida em <categorias>. 
+Você é meticuloso, eficiente e atento aos detalhes, garantindo precisão ao classificar transações. 
+Além disso, você organiza os dados de forma clara e lógica, facilitando a análise financeira e o planejamento do usuário.
+"""
+
+categorias = [
+    'casa',
+    'educacao',
+    'eletronicos',
+    'lazer',
+    'outros',
+    'restaurante',
+    'saude',
+    'servicos',
+    'supermercado',
+    'transporte',
+    'vestuario',
+    'viagem',
+]
+
+DADOS_CSV = '''
+date, title, amount
+2024-11-15, Supermercado Central, 250.75
+2024-11-17, Restaurante Sabor Caseiro, 120.90
+2024-11-18, Posto Alfa Combustíveis, 315.60
+2024-11-19, Farmácia Bem-Estar, 89.45
+2024-11-20, EletroShop, 1299.99
+2024-11-21, Livraria Cultura Viva, 58.30
+2024-11-22, Academia Boa Forma, 199.00
+2024-11-23, Loja de Roupas Elegance, 489.90
+2024-11-24, Cinema Top Mall, 65.50
+2024-11-25, Padaria Doce Pão, 34.20
+2024-11-26, Lanchonete FastBite, 42.50
+2024-11-27, Loja de Ferramentas Praticlar, 375.80
+2024-11-28, Pet Shop Vida Animal, 210.00
+2024-11-29, Loja de Brinquedos Alegria, 159.99
+2024-11-30, Mercado Orgânico Verde Vida, 98.75
+2024-12-01, Loja Online TecnoWorld, 2499.90
+2024-12-02, Café da Praça, 18.90
+2024-12-02, Assinatura StreamMax, 39.90
+2024-12-03, Oficina AutoPro, 940.00
+2024-12-04, Loja de Presentes Criativos, 135.70
+'''
+
+CATEGORIAS_PROMPT = '<categorias>' + ','.join(categorias) + '</categorias>'
+
+FATURA = '<fatura_csv>' + DADOS_CSV + '</fatura_csv>'
+
+PROMPT = "\n".join([PERSONA, CATEGORIAS_PROMPT, FATURA])
